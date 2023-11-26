21bd22a5e25cbe0626cf558cb67bb4624ce3701b
Mohammad Issawi <2000issawi@gmail.com>
2023-11-26 16:46:03 +0300

    Fix UVs for the textures

--- Assets/FragmentShader.fs
+++ Assets/FragmentShader.fs
@@ -3,5 +3,6 @@ uniform sampler2D albedo;
 out vec4 color;
 in vec2 pos_model_out;
 void main(){
-  color=texture(albedo,pos_model_out);
+  vec2 uv=(pos_model_out+vec2(1,1))/2.0;
+  color=texture(albedo,uv);
 }
